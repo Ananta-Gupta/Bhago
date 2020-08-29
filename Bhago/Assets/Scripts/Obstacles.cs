aaef887a209a2a1e985fe14ba82c6ad6f359caf0
Ananta Gupta <gupta.officials@gmail.com>
2020-08-29 15:06:03 +0530

    Player Manager, Obstacle Spawner Completed

---
+++ Bhago/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerManager>().health -= damage;
+            Debug.Log(collision.GetComponent<PlayerManager>().health);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Bhago/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float xincrement;
+
+    public float speed;
+    private readonly float minPos = -1.5f;
+    private readonly float maxPos = 1.5f;
+
+    public int health = 3;
+
+    private void Start()
+    {
+        targetPos = new Vector2(0, -2);
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minPos)
+        {
+            targetPos = new Vector2(transform.position.x - xincrement, -2);
+        }else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxPos)
+        {
+            targetPos = new Vector2(transform.position.x + xincrement, -2);
+        }
+    }
+}
